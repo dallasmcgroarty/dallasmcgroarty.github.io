fb79f37ab2c13cfae24397d2c43eea9f83faaca3
dallasmcgroarty <dallas.mcgroarty@gmail.com>
2019-06-18 19:59:29 -0700

    bio added, animated face added

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-(function (window) {
-  'use strict';
-})(window);
+//  function to loop through set of images
+(function () {
+// images array
+  var images = ['images/image1.png', 'images/image2.png', 'images/image3.png',
+    'images/image4.png', 'images/image5.png', 'images/image6.png', 'images/image7.png'];
+  var index = 0;
+  //  use setInterval function to loop through set of images on a timer
+  setInterval(function () {
+    if (images.length === index) {
+      index = 0;
+    } else {
+      console.log(images[index]);
+      var img = document.getElementById('face'); //  get img element
+      img.src = images[index]; //  change the image
+      index++;
+    }
+  }, 500);
+})();
