4955853d2a998b04d7f8be96850ab525a3b20e08
Dallas <dallas.mcgroarty@gmail.com>
2021-08-09 11:13:52 -0700

    Update scan.js

--- scan.js
+++ scan.js
@@ -1,26 +1,43 @@
 var btn = document.getElementById('start-scan');
 var span = document.getElementById('decoded-value');
 
-Quagga.init({
-    inputStream : {
-      name : "Live",
-      type : "LiveStream",
-      target: document.querySelector('body')
-    },
-    decoder : {
-      readers : ["code_128_reader"]
-    }
-  }, function(err) {
-      if (err) {
-          console.log(err);
-          return
-      }
-      console.log("Initialization finished. Ready to start");
-});
+// Quagga.init({
+//     inputStream : {
+//       name : "Live",
+//       type : "LiveStream",
+//       target: document.querySelector('body')
+//     },
+//     decoder : {
+//       readers : ["code_128_reader"]
+//     }
+//   }, function(err) {
+//       if (err) {
+//           console.log(err);
+//           return
+//       }
+//       console.log("Initialization finished. Ready to start");
+//       Quagga.start();
+// });
 
 btn.addEventListener('click', function (e) {
     e.preventDefault();
-    Quagga.start();
+    Quagga.init({
+      inputStream : {
+        name : "Live",
+        type : "LiveStream",
+        target: document.querySelector('body')
+      },
+      decoder : {
+        readers : ["code_128_reader"]
+      }
+    }, function(err) {
+        if (err) {
+            console.log(err);
+            return
+        }
+        console.log("Initialization finished. Ready to start");
+        Quagga.start();
+  });
 });
 
 Quagga.onDetected(function(result) {
