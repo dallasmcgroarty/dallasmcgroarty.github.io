da57416272b5edb0c421887544da5a6de9e3b8bc
Dallas <dallas.mcgroarty@gmail.com>
2021-08-09 10:59:31 -0700

    Add files via upload

---
+++ scan.js
@@ -0,0 +1,31 @@
+var btn = document.getElementById('start-scan');
+var span = document.getElementById('decoded-value');
+
+Quagga.init({
+    inputStream : {
+      name : "Live",
+      type : "LiveStream",
+      target: document.querySelector('body')
+    },
+    decoder : {
+      readers : ["code_128_reader"]
+    }
+  }, function(err) {
+      if (err) {
+          console.log(err);
+          return
+      }
+      console.log("Initialization finished. Ready to start");
+});
+
+btn.addEventListener('click', function (e) {
+    e.preventDefault();
+    Quagga.start();
+});
+
+Quagga.onDetected(function(result) {
+    var code = result.codeResult.code;
+    span.textContent = code;
+    console.log(code);
+    Quagga.stop();
+});
