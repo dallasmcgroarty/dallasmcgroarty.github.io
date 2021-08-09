0c3e457b62163f9c933d75531bc86cdbdc0050a9
Dallas <dallas.mcgroarty@gmail.com>
2021-08-09 11:21:27 -0700

    Update scan.js

--- scan.js
+++ scan.js
@@ -1,43 +1,34 @@
 var btn = document.getElementById('start-scan');
 var span = document.getElementById('decoded-value');
+var isScanOn = false;
 
-// Quagga.init({
-//     inputStream : {
-//       name : "Live",
-//       type : "LiveStream",
-//       target: document.querySelector('body')
-//     },
-//     decoder : {
-//       readers : ["code_128_reader"]
-//     }
-//   }, function(err) {
-//       if (err) {
-//           console.log(err);
-//           return
-//       }
-//       console.log("Initialization finished. Ready to start");
-//       Quagga.start();
-// });
+function startScan() {
+  Quagga.init({
+    inputStream : {
+      name : "Live",
+      type : "LiveStream",
+      target: document.querySelector('body')
+    },
+    decoder : {
+      readers : ["code_128_reader"]
+    }
+  }, function(err) {
+      if (err) {
+          console.log(err);
+          return
+      }
+      console.log("Initialization finished. Ready to start");
+      Quagga.start();
+      scanRunning = true;
+  });
+}
 
 btn.addEventListener('click', function (e) {
-    e.preventDefault();
-    Quagga.init({
-      inputStream : {
-        name : "Live",
-        type : "LiveStream",
-        target: document.querySelector('body')
-      },
-      decoder : {
-        readers : ["code_128_reader"]
-      }
-    }, function(err) {
-        if (err) {
-            console.log(err);
-            return
-        }
-        console.log("Initialization finished. Ready to start");
-        Quagga.start();
-  });
+  if (isScanOn) {
+    Quagga.stop()
+  } else {
+    startScan();
+  }
 });
 
 Quagga.onDetected(function(result) {
@@ -46,3 +37,4 @@ Quagga.onDetected(function(result) {
     console.log(code);
     Quagga.stop();
 });
+
