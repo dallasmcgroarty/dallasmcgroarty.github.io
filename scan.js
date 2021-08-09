d9611da083d965cddfa3c902bac8fac62dbc9f34
Dallas <dallas.mcgroarty@gmail.com>
2021-08-09 11:58:58 -0700

    Update scan.js

--- scan.js
+++ scan.js
@@ -31,7 +31,7 @@ btn.addEventListener('click', function (e) {
     startScan();
   }
   
-  hideShowCamera();
+  video.style.display = 'block';
 });
 
 Quagga.onDetected(function(result) {
@@ -40,13 +40,5 @@ Quagga.onDetected(function(result) {
     console.log(code);
   
     Quagga.stop();
-    hideShowCamera();
-});
-
-function hideShowCamera() {
-  if (video.style.display == 'none') {
-    video.style.display = 'block';
-  } else {
     video.style.display = 'none';
-  }
-}
+});
